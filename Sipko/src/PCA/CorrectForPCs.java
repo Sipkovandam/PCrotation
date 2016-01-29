1404c419f0a03a194217afae3d1ed4a061d8e6db
Sipko <sipkovandam@gmail.com>
2016-01-29 16:12:31 +0100

    Update

--- Sipko/src/PCA/CorrectForPCs.java
+++ Sipko/src/PCA/CorrectForPCs.java
@@ -10,21 +10,26 @@ public class CorrectForPCs {
 
 	public static void main(String[] args) throws IOException 
 	{
-		String expFile = "E:/Groningen/Data/PublicSamples/100SamplesTest/Rsample/" + "TESTexpression.txt";
+		String sampleFile = "E:/Groningen/Data/PublicSamples/100SamplesTest/Rsample/" + "RandomSamples.txt";
 		String vectorFolder = "E:/Groningen/Data/PublicSamples/100SamplesTest/Rsample/TESTexpression/";
+		
+		//String expFile = "E:/Groningen/Data/PublicSamples/ComputerTest/Rtest/" + "rSample1.txt";
+		//String vectorFolder = "E:/Groningen/Data/PublicSamples/ComputerTest/Rtest/rSample/";
+		//
+		
 		String PCsToAdjust = "0";//calling this function with 0 will just rotate the matrix back without adjusting for any PCs.
 		
 		checkArgs(args);
-		if(!System.getProperty("user.dir").contains("E:\\Groningen\\Workspace"))
+		if(!System.getProperty("user.dir").contains("C:\\Users\\Sipko\\git\\PCrotation\\Sipko"))
 		{
-			expFile = args[0];
+			sampleFile = args[0];
 			vectorFolder = args[1]+"/";
 			PCsToAdjust = args[2];
 		}
 		
-		String writeFolder = expFile.replace(".txt", "_Adj/");
+		String writeFolder = sampleFile.replace(".txt", "_Adj/");
 		
-		MatrixStruct[] PCscoresSample = RotateSample.rotate(expFile, vectorFolder, writeFolder);
+		MatrixStruct[] PCscoresSample = RotateSample.rotate(sampleFile, vectorFolder, writeFolder);
 		/**set PCs to 0 (correcting for principal components)**/
 		ArrayList<Integer> PCsToCorrect = parsePCs(PCsToAdjust);
 		//int[] PCsToCorrect = new int[]{1,2,3,4,5};
@@ -40,20 +45,26 @@ public class CorrectForPCs {
 		String saveNameSingleRotatedBack = writeFolder + "SAMPLE_RotatedBack.txt";
 		MatrixStruct geneEigenVectors = new MatrixStruct(vectorFolder+"GENE.eigenvectors.txt");
 		MatrixStruct[] rotatedBack = PCA.rotateBack(geneEigenVectors,PCscoresSample[0], null, saveNameSingleRotatedBack);
-		/**Add averages that were initially removed**/
-		pca.PCA.log("12. Adding column averages again");
-		rotatedBack[0].addAveragesCols(new MatrixStruct(vectorFolder+"SAMPLE_QuantNorm_columnAverages.txt"));
-		pca.PCA.log("13. Adding row averages again");
-		rotatedBack[0].addAveragesRows(new MatrixStruct(vectorFolder+"SAMPLE_QuantNorm_rowAverages.txt"));
-		String writeName = writeFolder+"Adjusted_PC"+PCsToAdjust+".txt";
-		pca.PCA.log("14. Writing adjusted file to:");
-		rotatedBack[0].write(writeName);
-		pca.PCA.log("File written to: " + writeName);
-		pca.PCA.log("15. Transposing adjusted file");
+		
+		String rotatedAdjusted = writeFolder+"Adjusted_PC"+PCsToAdjust+".txt";;
+		rotatedBack[0].write(rotatedAdjusted);
 		rotatedBack[0].transpose();
-		String transposedWriteName = writeName.replace(".txt", "_transposed.txt"); 
-		pca.PCA.log("16. Writing transposed file to" + transposedWriteName);
-		rotatedBack[0].write(transposedWriteName);
+		rotatedBack[0].write(rotatedAdjusted.replace(".txt","_transposed.txt"));
+		pca.PCA.log("Corrected file written to:" + rotatedAdjusted);
+//		/**Add averages that were initially removed**/
+//		pca.PCA.log("12. Adding column averages again");
+//		rotatedBack[0].addAveragesCols(new MatrixStruct(vectorFolder+"SAMPLE_QuantNorm_columnAverages.txt"));
+//		pca.PCA.log("13. Adding row averages again");
+//		rotatedBack[0].addAveragesRows(new MatrixStruct(writeFolder+"rowAverages.txt"));
+//		String writeName = writeFolder+"Adjusted_PC"+PCsToAdjust+"_Uncentered.txt";
+//		pca.PCA.log("14. Writing adjusted file to:");
+//		rotatedBack[0].write(writeName);
+//		pca.PCA.log("File written to: " + writeName);
+//		pca.PCA.log("15. Transposing adjusted file");
+//		rotatedBack[0].transpose();
+//		String transposedWriteName = writeName.replace(".txt", "_transposed.txt"); 
+//		pca.PCA.log("16. Writing transposed file to" + transposedWriteName);
+//		rotatedBack[0].write(transposedWriteName);
 	}
 
 	public static ArrayList<Integer> parsePCs(String PCsToAdjust) 
@@ -93,7 +104,7 @@ public class CorrectForPCs {
 	}
 	public static void checkArgs(String[] args)
 	{
-		if(System.getProperty("user.dir").contains("E:\\Groningen\\Workspace"))
+		if(System.getProperty("user.dir").contains("C:\\Users\\Sipko\\git\\PCrotation\\Sipko"))
 			return;
 		if(args.length != 3)
 		{
